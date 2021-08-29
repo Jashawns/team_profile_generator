495758c24df6ad9dd74ece39154d233e0a9d26c1
Jashawns <shawny583@hotmail.com>
2021-08-28 21:04:51 -0400

    javascriot

--- index.js
+++ index.js
@@ -37,3 +37,74 @@ function entries() {
             },
         ])
         .then(
+            function ({ name, id, email, role }) {
+                if (role === "Engineer") {
+                        inquirer
+                            .prompt({
+                                type: "response",
+                                name: "github",
+                                message: "What is the team member's GitHub username?",
+                            }).then(
+                                function ({ github }) {
+                                    createEngineer(name, id, email, github)
+                                    teamMembers()
+                                }
+                            )
+                } else if (role === "Intern") {
+                        inquirer
+                            .prompt({
+                                type: "response",
+                                name: "school",
+                                message: "What school does the team member attend?",
+                            }).then(
+                                function ({ school }) {
+                                    createIntern(name, id, email, school)
+                                    teamMembers()
+                                }
+                            )
+                } else {
+                        inquirer
+                            .prompt({
+                                type: "number",
+                                name: "officeNumber",
+                                message: "What is the team member's Office Number?",
+                            }).then(
+                                function ({ officeNumber }) {
+                                    createManager(name, id, email, officeNumber)
+                                    teamMembers()
+                                }
+                            )
+                }
+            })
+
+}
+
+function teamMembers() {
+    inquirer.prompt(
+    {
+        type: "list",
+        name: "teamMembers",
+        message: "Add more team members?",
+        choices: [
+            "Yes",
+            "No"
+        ]
+    })
+    .then(
+        function ({ teamMembers }) {
+            console.log("Additional:", teamMembers)
+            if (teamMembers === "Yes") {
+                entries();
+            } else {     
+                processHTML()
+            }
+        }
+    )
+        .catch(err => {
+            console.log("Error", err)
+            throw err
+        })
+}
+
+
+entries()
